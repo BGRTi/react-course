9be9304018f5523f33598174b5d5f7d3653d6002
Aliaksei Rachanok <bagarottiz@gmail.com>
2019-05-08 15:26:22 +0300

    Root comp cahnged, hydrate added serv updated

---
+++ serverRendered.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import Root from './Root';
+import store from './store';
+
+function renderHTML(html, preloadedState) {
+  return `
+      <!doctype html>
+      <html>
+        <head>
+          <meta charset=utf-8>
+          <title>React Server Side Rendering</title>
+          ${process.env.NODE_ENV === 'development' ? '' : '<link href="/css/main.css" rel="stylesheet" type="text/css">'}
+        </head>
+        <body>
+          <div id="root">${html}</div>
+          <script>
+            window.PRELOADED_STATE = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+          </script>
+          <script src="/js/main.js"></script>
+        </body>
+      </html>
+  `;
+}
+
+export default function serverRenderer() {
+  return (req, res) => {
+    // This context object contains the results of the render
+    const context = {};
+
+    const renderRoot = () => (
+      <Root
+        context={context}
+        location={req.url}
+        Router={StaticRouter}
+        store={store}
+      />
+    );
+
+      const htmlString = renderToString(renderRoot());
+
+      if (context.url) {
+        res.writeHead(302, {
+          Location: context.url,
+        });
+        res.end();
+        return;
+      }
+
+      res.send(renderHTML(htmlString, preloadedState));
+
+    renderToString(renderRoot());
+  };
+}
